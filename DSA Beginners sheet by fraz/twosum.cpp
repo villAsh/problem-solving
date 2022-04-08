cb285252295fa4aa398ae84fe7b729334dd5b661
villAsh <50195101+villAsh@users.noreply.github.com>
2022-04-08 11:07:54 +0530

    Update twosum.cpp

--- DSA Beginners sheet by fraz/twosum.cpp
+++ DSA Beginners sheet by fraz/twosum.cpp
@@ -1,1 +1,17 @@
+this is simple brute force approach;
 
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+      int n = int(nums.size()) - 1;
+        for(int i =0;i < n-1;i++){
+            for(int j = i+1;j < n;j++){
+                if(nums[i]+nums[j] == target){
+                    return {i,j};
+                }
+            }
+        }
+        return {-1,-1};
+    }
+};
